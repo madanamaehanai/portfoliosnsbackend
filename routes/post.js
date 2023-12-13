9bc13c037f2e21c1b60a38a2c5f651a3cfabe72f
madanamaehanai <homehack@outlook.jp>
2023-12-13 15:24:04 +0900

    usergetadd

--- routes/post.js
+++ routes/post.js
@@ -17,18 +17,28 @@ router.post("/", async (req, res) => {
     }
 });
 
+//ユーザーを取得する。
+router.get("/author", async (req, res) => {
+    try {
+        const getauthor = await User.find(({}));
+        return res.status(200).json(getauthor)
+    } catch (err) {
+        return res.status(500).json(err);
+    }
+});
+
 
 //全ての投稿を取得
 router.get("/timeline/:userId", async (req, res) => {
     try {
         const currentUser = await User.findById(req.params.userId);
         const userPosts = await Post.find({ userId: currentUser._id });
-          const friendPosts = await Promise.all(
-            currentUser.followings.map((friendId) => {  
-              return Post.find({ userId: friendId });
+        const friendPosts = await Promise.all(
+            currentUser.followings.map((friendId) => {
+                return Post.find({ userId: friendId });
             })
-          );
-          res.status(200).json(userPosts.concat(...friendPosts));
+        );
+        res.status(200).json(userPosts.concat(...friendPosts));
         // res.status(200).json(currentUser);
     } catch (err) {
         res.status(500).json(err);
