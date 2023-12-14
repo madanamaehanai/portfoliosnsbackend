6897c55b7b9067d2ef97da60d4b4e56369536e8d
madanamaehanai <homehack@outlook.jp>
2023-12-14 16:33:12 +0900

    会社スキーマの作成

---
+++ schema/Company.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+
+
+const CompanySchema = new mongoose.Schema({
+    companyname: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    url: {
+        type: String,
+        required: true,
+        uniqu: true,
+    },
+    service: {
+        type: Array,
+        default: [],
+    },
+    desc: {
+        type: String,
+        default: "",
+    },
+    language: {
+        type: Array,
+        default: [],
+    },
+    skill: {
+        type: Array,
+        default: [],
+    },
+    chat: {
+        type: Array,
+        default: [],
+    },
+},
+    //時間を格納
+    { timestamps: true }
+);
+
+module.exports = mongoose.model("Company", CompanySchema);
