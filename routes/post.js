7f731e7c46b5889884749a0b317a1801f798cbe5
madanamaehanai <homehack@outlook.jp>
2023-12-14 15:39:39 +0900

    postのデバック

--- routes/post.js
+++ routes/post.js
@@ -34,7 +34,7 @@ router.get("/timeline/:userId", async (req, res) => {
         const currentUser = await User.findById(req.params.userId);
         const userPosts = await Post.find({ userId: currentUser._id });
         const friendPosts = await Promise.all(
-            currentUser.followings.map((friendId) => {
+            currentUser.followers.map((friendId) => {
                 return Post.find({ userId: friendId });
             })
         );
