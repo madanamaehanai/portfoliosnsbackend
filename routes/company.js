d9cffcdcb6372fb7a1807e7d3c9d30ec29297068
madanamaehanai <homehack@outlook.jp>
2023-12-17 13:53:42 +0900

    add

--- routes/company.js
+++ routes/company.js
@@ -83,4 +83,16 @@ router.post("/company/services/:company_id", async (req, res) => {
 });
 
 
+//会社サービス情報を取得
+router.get("/company/services/:id", async (req, res) => {
+    try {
+            const companyservicesPosts = await Coservices.find({ id: req.params._id }).exec();
+            res.status(200).json({
+                companyservicesPosts
+            });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 module.exports = router;
