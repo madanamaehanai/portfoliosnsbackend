c7368e08454d716ea7fc1c7d39b1ff0a05953a83
madanamaehanai <homehack@outlook.jp>
2023-12-25 10:43:49 +0900

    コンフリクション対応

--- routes/company.js
+++ routes/company.js
@@ -6,7 +6,6 @@ const Company = require("../schema/Company");
 const Coservices = require("../schema/Coservices");
 
 
-
 //会社プロフィールの作成
 router.post("/company", async (req, res) => {
     const newCompany = new Company(req.body);
@@ -19,7 +18,7 @@ router.post("/company", async (req, res) => {
 });
 
 //会社プロフィールの取得
-router.get("/company/:id", async (req, res) => {
+router.get("/companyinfo/:id", async (req, res) => {
     try {
         const CompanyInfo = await Company.findById(req.params.id);
         return res.status(200).json(CompanyInfo);
@@ -31,41 +30,21 @@ router.get("/company/:id", async (req, res) => {
 //カテゴリー毎の会社情報を取得
 router.get("/company/:categoryname", async (req, res) => {
     try {
-        // const companyPosts = await Company.find({ category: req.params.categoryname }).exec();
-        // res.status(200).json(companyPosts);
-
-        // if (companyPosts.length > 0) {
-        // const companyIds = company.service.map(service => service._id);
-
-        // const coservicesPosts = await Coservices.find({ _id: { $in: companyIds } }).exec();
-
         const companyPosts = await Company.find({ category: req.params.categoryname }).exec();
 
-        // if (companyPosts.length > 0) {
-        //     const serviceIds = companyPosts.map(company => company.service.map(service => service._id)).flat();
-        //     const services = [];
-
-        //     for (const serviceId of serviceIds) {
-        //         const coservice = await Coservices.findById(serviceId);
-        //         services.push(coservice);
-        //     }
-
-
-
         res.status(200).json({
-            // companyPosts,
-            // coservicesPosts
-            // companyIds
             companyPosts
-            // services
         });
-        // } else {
-        //     res.status(404).json({ message: 'No company found for the specified category' });
-        // }
     } catch (err) {
         res.status(500).json(err);
     }
 });
+
+
+
+
+
+
 
 //会社サービスを追加
 router.post("/company/services/:company_id", async (req, res) => {
